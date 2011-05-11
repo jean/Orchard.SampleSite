79c8fccfd3255447f1a6d8c695c594f20f9d813f
ukdevcom <ukdevcom@localhost>
2011-05-11 18:19:21 +0100

    Added synopsis part to product types

---
+++ final/Modules/SampleSiteModule/Models/SynopsisRecord.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.ContentManagement.Records;
+using Orchard.Environment.Extensions;
+
+namespace SampleSiteModule.Models
+{
+	[OrchardFeature("Synopsis")]
+	public class SynopsisRecord : ContentPartRecord
+	{
+		public virtual string SynopsisText { get; set; }
+		public virtual string ThumbnailUrl { get; set; }
+	}
+}
