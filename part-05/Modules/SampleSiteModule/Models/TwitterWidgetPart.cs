afe442539a6f1a9682e109dbe4d01d6e663af69c
Tony Johnson <Deepcodecouk@users.noreply.github.com>
2011-05-17 16:48:34 +0100

    Added part-05 - clean up the twitter widget

---
+++ part-05/Modules/SampleSiteModule/Models/TwitterWidgetPart.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+
+namespace SampleSiteModule.Models
+{
+	[OrchardFeature("TwitterWidget")]
+	public class TwitterWidgetPart : ContentPart<TwitterWidgetRecord>
+	{
+		[Required]
+		public string TwitterUserName
+		{
+			get { return Record.TwitterUser; }
+			set { Record.TwitterUser = value; }
+		}
+
+		[Required]
+		[DefaultValue(5)]
+		public int MaxPosts
+		{
+			get { return Record.MaxPosts; }
+			set { Record.MaxPosts = value; }
+		}
+
+		[Required]
+		[DefaultValue(60)]
+		public int CacheMinutes
+		{
+			get { return Record.CacheMinutes; }
+			set { Record.CacheMinutes = value; }
+		}
+	}
+}
