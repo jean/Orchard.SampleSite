d112aae5e24671699af7940abe68bcc59d32cfb5
ukdevcom <ukdevcom@localhost>
2011-05-17 15:09:49 +0100

    Added code for part-04 of the blog series

---
+++ part-04/Modules/SampleSiteModule/Services/ITwitterService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Orchard;
+using Orchard.Environment.Extensions;
+using SampleSiteModule.Models;
+
+namespace SampleSiteModule.Services
+{
+	public interface ITwitterService : IDependency
+	{
+		IList<Tweet> GetLatestTweetsFor(TwitterWidgetPart part);
+	}
+}
