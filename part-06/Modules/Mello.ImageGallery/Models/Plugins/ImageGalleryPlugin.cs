4c6fa589509d5758ed533d6189a7745341ddd054
Tony Johnson <Deepcodecouk@users.noreply.github.com>
2011-05-31 21:26:57 +0100

    Added part-06 to accompany blog post

---
+++ part-06/Modules/Mello.ImageGallery/Models/Plugins/ImageGalleryPlugin.cs
@@ -0,0 +1,13 @@
+﻿namespace Mello.ImageGallery.Models.Plugins {
+    public abstract class ImageGalleryPlugin {
+        public abstract string ToString(string cssSelector);
+
+        public virtual string AdditionalHrefMarkup {
+            get { return string.Empty; }
+        }
+
+        public virtual string ImageGalleryTemplateName {
+            get { return "Parts/ImageGallery"; }
+        }
+    }
+}
---
+++ part-06/Modules/SampleSiteModule/Models/SynopsisPart.cs
@@ -0,0 +1,21 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+
+namespace SampleSiteModule.Models
+{
+	[OrchardFeature("Synopsis")]
+	public class SynopsisPart : ContentPart<SynopsisRecord>
+	{
+		public string Synopsis
+		{
+			get { return Record.SynopsisText; }
+			set { Record.SynopsisText = value; }
+		}
+
+		public string ThumbnailUrl
+		{
+			get { return Record.ThumbnailUrl; }
+			set { Record.ThumbnailUrl = value; }
+		}
+	}
+}
